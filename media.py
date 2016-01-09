e4946b50d4cb7b48dd18d62425aa9e346cc2c6cf
Wilbert <wilbertcr@gmail.com>
2016-01-09 14:07:29 -0800

    Added file provided "fresh_tomatoes.py". Started Movie Class and its test file.

---
+++ media.py
@@ -0,0 +1,7 @@
+class Movie():
+    def __init__(self,title,storyline,poster_image,youtube_trailer):
+        self.title = title
+        self.storyline = storyline
+        self.poster_image = poster_image
+        self.youtube_trailer = youtube_trailer
+

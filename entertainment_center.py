d6a2c2c536081677e093a87f947d0f4c5ead6a28
Wilbert <wilbertcr@gmail.com>
2016-01-11 12:09:20 -0800

    Fixed pep8 issues with code.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,14 +1,16 @@
 import fresh_tomatoes
 import media
-import tmdbsimple as tmdb
 import tmdb_key
+import tmdbsimple as tmdb
+
 tmdb = tmdb_key.set_key(tmdb)
 
 # Get Search Object
 search = tmdb.Search()
 
 # We need to get the base path for the movie posters.
-# See http://docs.themoviedb.apiary.io/#reference/configuration/configuration/get for information on what's going on here.
+# See http://docs.themoviedb.apiary.io/#reference/configuration/configuration/get
+#  for information on what's going on here.
 information = tmdb.Configuration().info()
 images_info = information['images']
 IMG_BASE_URL = images_info['secure_base_url']
@@ -22,7 +24,7 @@ YOUTUBE_BASE_PATH = "https://www.youtube.com/watch?v="
 movies_titles = []
 
 # Ask the user for the titles.
-while (True):
+while True:
     movie = raw_input("Please enter the title of a movie, or \"exit\" to complete your selection.\n")
     if movie.lower() == "exit":
         break
@@ -37,20 +39,21 @@ movie_list = []
 # For each movie in the list
 for movie in movies_titles:
 
-    #Let's query the DB.
+    # Let's query the DB.
 
     response = search.movie(query=movie)
 
-    #And let's use the first result to populate attributes.
+    # And let's use the first result to populate attributes.
 
     for s in response['results']:
         try:
-            id = s["id"]
+
+            movie_id = s["id"]
             title = s["title"]
             overview = s["overview"]
             poster_path = POSTER_BASE_PATH + s["poster_path"]
-            movies = tmdb.Movies(id=id)
-            trailers = movies.videos(id=id)
+            movies = tmdb.Movies(id=movie_id)
+            trailers = movies.videos(id=movie_id)
             trailer = trailers["results"][0]
             youtube_path = "Test"
             youtube_path = YOUTUBE_BASE_PATH + trailer["key"]
